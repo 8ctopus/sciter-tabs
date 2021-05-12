8b18ea0d29aedbe58f0b28ac91c86212540716a5
8ctopus <hello@octopuslabs.io>
2021-05-12 11:31:30 +0500

    fixed: sciter sdk 4.4.7.4 F5 refresh is broken, downgrade to 4.4.7.3

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-REM sciter 4.4.7.4
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b0250adeff6bcf4da7c6489c95c05d5544cb003/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b0250adeff6bcf4da7c6489c95c05d5544cb003/bin/windows/x32/sciter.dll
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/4b0250adeff6bcf4da7c6489c95c05d5544cb003/bin/windows/x32/inspector.exe
+REM sciter 4.4.7.3
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/e88319855963794e84fd9cfb210c4f97b956eb72/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/e88319855963794e84fd9cfb210c4f97b956eb72/bin/windows/x32/sciter.dll
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/e88319855963794e84fd9cfb210c4f97b956eb72/bin/windows/x32/inspector.exe
 
 pause
