5e75dabee354e727b2d993b4d9e6ec1248e17eef
8ctopus <hello@octopuslabs.io>
2021-11-19 11:47:51 +0500

    added: linux and mac support updated: sciter sdk to 4.4.8.17bis

--- start.bat
+++ start.bat
@@ -1,9 +1,12 @@
 tasklist /fi "ImageName eq inspector.exe" /fo csv 2>NUL | find /I "inspector.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM inspector.exe /f
 
-start inspector.exe
+start bin\win-x32\inspector.exe
+
+REM wait for inspector to initialize
+sleep 1
 
 tasklist /fi "ImageName eq scapp.exe" /fo csv 2>NUL | find /I "scapp.exe">NUL
 if "%ERRORLEVEL%"=="0" taskkill /IM scapp.exe /f
 
-start scapp.exe main.html --debug
+start bin\win-x32\scapp.exe main.html --debug
